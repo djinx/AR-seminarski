f19477d040dec0eabe3b8c80ed8bd39b6ddfda7c
djinx <key13043@gmail.com>
2017-05-01 14:54:37 +0200

    Napisane funkcije oppositeLiteral, litFromVar, varFromLit

--- Makefile
+++ Makefile
@@ -1,16 +1,13 @@
-PROGRAM=dp
+PROGRAM   = dp
 CXX       = g++
-CXXFLAGS  = -g -std=c++0x -Wall
+CXXFLAGS  = -g -std=c++11 -Wall
 
 $(PROGRAM): dp.o
 	$(CXX) -o $(PROGRAM) dp.o
 
-.PHONY: clean dist
+.PHONY: clean
 
 clean:
-	-rm *.o $(PROGRAM) *core
-
-dist: clean
-	-tar -chvj -C .. -f ../$(PROGRAM).tar.bz2 $(PROGRAM)
+	rm *.o $(PROGRAM) *core
 
 
--- dp.cpp
+++ dp.cpp
@@ -1,7 +1,40 @@
 #include "dp.hpp"
 
 
+Literal litFromVar(Variable v, Polarity p) {
+  return p == POSITIVE ? v << : (v << 1) | 1;
+}
+
+Variable varFromLit(Literal l){
+  return l >> 1;
+}
+
+bool isPositive(Literal l);
+
+bool isNegative(Literal l);
+
+Literal oppositeLiteral(Literal l) {
+  return l ^ 1;
+}
+
+typedef vector<Literal> Clause;
+typedef vector<Clause> Formula;
+
+
+DPSolve::DPSolve(const Formula & f, unsigned num);
+
+bool DPSolve::contains(const Clause & c, Literal l);
+
+bool DPSolve::DPSolve::resolution(Literal l, Clause & c1, Clause & c2, Clause & r);
+
+bool DPSolve::eliminate(Variable v);
+
+bool DPSolve::checkIfSat();
+
+
 int main () {
+  
+  
   
   cout << "Ne radi nistaa!! " << endl;
   return 0;
